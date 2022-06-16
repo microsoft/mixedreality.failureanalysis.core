4c85554e2ce85aa8f5e5d53d3e5314040f1e52a9
Andrew McClelland <andrew.mcclelland@microsoft.com>
2022-06-16 12:58:47 -0700

    INFRA: Initialize API Project

---
+++ FailureAnalysis.Core.Api/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace FailureAnalysis.Core.Api.Controllers
+{
+    
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get() =>
+            Ok("Hello, Mario. The princess is in another castle!");
+    }
+}
---
+++ FailureAnalysis.Core.Api/Startup.cs
@@ -0,0 +1,57 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Text.Json.Serialization;
+using Microsoft.OpenApi.Models;
+
+namespace FailureAnalysis.Core.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers().AddJsonOptions(options =>
+                options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+
+            services.AddHttpClient();
+            services.AddLogging();
+
+            services.AddSwaggerGen(option =>
+            {
+                option.SwaggerDoc(
+                    name: "v1",
+                    info: new OpenApiInfo
+                    {
+                        Title = "FailureAnalysis.Core.Api",
+                        Version = "v1"
+                    });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint(
+                        url: "/swagger/v1/swagger.json",
+                        name: "FailureAnalysis.Core.Api v1");
+                });
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
+        }
+    }
+}
