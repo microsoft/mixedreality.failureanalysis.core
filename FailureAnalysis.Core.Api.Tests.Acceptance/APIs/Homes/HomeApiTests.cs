330dc6c6aeeb92d47c67e8cee586bafd6870eccf
Terry Palomares <tpalomares18@gmail.com>
2022-06-16 14:16:53 -0700

    INFRA: Initialize Acceptance Project

---
+++ FailureAnalysis.Core.Api.Tests.Acceptance/APIs/Homes/HomeApiTests.cs
@@ -0,0 +1,35 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FailureAnalysis.Core.Api.Tests.Acceptance.Brokers;
+using FluentAssertions;
+using Xunit;
+
+namespace FailureAnalysis.Core.Api.Tests.Acceptance.APIs.Homes
+{
+    [Collection(nameof(ApiTestCollection))]
+    public class HomeApiTests
+    {
+        private readonly FailureAnalysisCoreApiBroker dmxGatekeeperApiBroker;
+
+        public HomeApiTests(FailureAnalysisCoreApiBroker dmxGatekeeperApiBroker) =>
+            this.dmxGatekeeperApiBroker = dmxGatekeeperApiBroker;
+
+        [Fact]
+        public async Task ShouldReturnHomeMessageAsync()
+        {
+            string expectedMessage = "Hello, Mario. The princess is in another castle!";
+
+            string actualMessage =
+                await this.dmxGatekeeperApiBroker.GetHomeMessageAsync();
+
+            actualMessage.Should().BeEquivalentTo(expectedMessage);
+        }
+    }
+}
