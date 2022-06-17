452802f2954f3273cd914b659633ff0a7f9771b6
Andrew McClelland <andrew.mcclelland@microsoft.com>
2022-06-17 09:56:01 -0700

    INFRA: Call CloudManagementProcessingService from Program

--- FailureAnalysis.Core.Api.Infrastructure.Provision/Program.cs
+++ FailureAnalysis.Core.Api.Infrastructure.Provision/Program.cs
@@ -1,3 +1,18 @@
 ﻿// ---------------------------------------------------------------
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FailureAnalysis.Core.Api.Infrastructure.Provision.Services.Processings.CloudManagements;
+
+namespace FailureAnalysis.Core.Api.Infrastructure.Provision
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var cloudManagementProcessingService = new CloudManagementProcessingService();
+            await cloudManagementProcessingService.ProcessAsync();
+        }
+    }
+}
