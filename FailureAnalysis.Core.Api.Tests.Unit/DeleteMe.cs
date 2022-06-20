4406caccb9e44a0ea3c8c71ae1be43e3042404e0
Geetika Jain <geetsk2194@gmail.com>
2022-06-20 11:02:40 -0700

    INFRA: Initialize Unit tests project

---
+++ FailureAnalysis.Core.Api.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,17 @@
+// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using Xunit;
+
+namespace FailureAnalysis.Core.Api.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
