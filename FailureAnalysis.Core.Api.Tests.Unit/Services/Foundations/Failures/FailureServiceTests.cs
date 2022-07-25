4a5488c9b23ca4be150596ee36adb6aba63b9bd0
Andrew McClelland <andrew.mcclelland@microsoft.com>
2022-07-25 10:39:16 -0700

    ShouldAddFailureAsync -> FAIL

---
+++ FailureAnalysis.Core.Api.Tests.Unit/Services/Foundations/Failures/FailureServiceTests.cs
@@ -0,0 +1,47 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using FailureAnalysis.Core.Api.Brokers;
+using FailureAnalysis.Core.Api.Brokers.Loggings;
+using FailureAnalysis.Core.Api.Models;
+using FailureAnalysis.Core.Api.Services.Foundations.Failures;
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace FailureAnalysis.Core.Api.Tests.Unit.Services.Foundations.Failures
+{
+    public partial class FailureServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly IFailureService failureService;
+
+        public FailureServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.failureService = new FailureService(
+                storageBroker: storageBrokerMock.Object,
+                loggingBroker: loggingBrokerMock.Object);
+        }
+
+        private static Failure CreateRandomFailure() =>
+            CreateFailureFiller().Create();
+
+        private static Filler<Failure> CreateFailureFiller()
+        {
+            var filler = new Filler<Failure>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(GetRandomDateTimeOffset());
+
+            return filler;
+        }
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+    }
+}
---
+++ FailureAnalysis.Core.Api/Services/Foundations/Failures/FailureService.cs
@@ -0,0 +1,29 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using FailureAnalysis.Core.Api.Brokers;
+using FailureAnalysis.Core.Api.Brokers.Loggings;
+using FailureAnalysis.Core.Api.Models;
+
+namespace FailureAnalysis.Core.Api.Services.Foundations.Failures
+{
+    public class FailureService : IFailureService
+    {
+        private readonly IStorageBroker storageBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public FailureService(
+            IStorageBroker storageBroker,
+            ILoggingBroker loggingBroker)
+        {
+            this.storageBroker = storageBroker;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public ValueTask<Failure> AddFailureAsync(Failure failure)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
