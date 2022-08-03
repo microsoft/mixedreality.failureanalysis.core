5ce0af16d3fc6040d908bde503e0608e4a29e07c
Geetika Jain <geetsk2194@gmail.com>
2022-08-02 17:44:53 -0700

    ACCEPTANCE: Post Failure

---
+++ FailureAnalysis.Core.Api.Tests.Acceptance/APIs/Failures/FailureApiTests.Logic.cs
@@ -0,0 +1,31 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FailureAnalysis.Core.Api.Models.Failures;
+using FluentAssertions;
+using Force.DeepCloner;
+using Xunit;
+
+namespace FailureAnalysis.Core.Api.Tests.Acceptance.APIs.Failures
+{
+    public partial class FailureApiTests
+    {
+        [Fact]
+        public async Task ShouldAddFailureAsync()
+        {
+            // given
+            Failure randomFailure = CreateRandomFailure();
+            var inputFailure = randomFailure;
+            var expectedFailure = randomFailure.DeepClone();
+
+            // when
+            Failure actualFailure = 
+                await this.failureAnalysisApiBroker.PostFailureAsync(inputFailure);
+
+            // then
+            actualFailure.Should().BeEquivalentTo(expectedFailure);
+        }
+    }
+}
---
+++ FailureAnalysis.Core.Api.Tests.Acceptance/APIs/Failures/FailureApiTests.cs
@@ -0,0 +1,39 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using FailureAnalysis.Core.Api.Models.Failures;
+using FailureAnalysis.Core.Api.Tests.Acceptance.Brokers;
+using Tynamix.ObjectFiller;
+using Xunit;
+
+namespace FailureAnalysis.Core.Api.Tests.Acceptance.APIs.Failures
+{
+    [Collection(nameof(ApiTestCollection))]
+    public partial class FailureApiTests
+    {
+        private readonly FailureAnalysisCoreApiBroker failureAnalysisApiBroker;
+
+        public FailureApiTests(FailureAnalysisCoreApiBroker failureAnalysisApiBroker) =>
+            this.failureAnalysisApiBroker = failureAnalysisApiBroker;
+
+        private static Failure CreateRandomFailure() =>
+            CreateRandomFailureFiller().Create();
+
+        private static Filler<Failure> CreateRandomFailureFiller()
+        {
+            var filler = new Filler<Failure>();
+            DateTimeOffset now = DateTimeOffset.UtcNow;
+            Guid id = Guid.NewGuid();
+
+            filler.Setup()
+                .OnProperty(failure => failure.Id).Use(id)
+                .OnProperty(failure => failure.ProfileId).Use(id)
+                .OnProperty(failure => failure.CreatedDate).Use(now)
+                .OnProperty(failure => failure.UpdatedDate).Use(now);
+
+            return filler;
+        }
+    }
+}
